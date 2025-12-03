3f6e5f286faad81528bef633a9f16d6705267ffb
DevJonasMax <jonas.max.dev@gmail.com>
2025-12-03 17:55:00 -0400

    docs(tasks): add comment to updateTask function
    Explain the purpose of the updateTask function for better code understanding

--- frontend/app/(pages)/app/actions/tasks.ts
+++ frontend/app/(pages)/app/actions/tasks.ts
@@ -10,7 +10,7 @@ export async function createTask(task: CreateTask) {
     const response = await Api.post<Task>("/api/tasks/create", task);
     return response.data;
 }
-
+// Update a task by ID
 export async function updateTask(id: string, taskUpdate: UpdateTask) {
     const response = await Api.patch<Task>(`/api/tasks/update/${id}`, taskUpdate);
     return response.data;
